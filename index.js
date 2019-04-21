f8da42869707a90c6d34d4e6de13cb67d98d3804
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-04-21 18:53:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+newHeader.innerText = "YOUR-NAME is the champion"
